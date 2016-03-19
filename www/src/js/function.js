a54901264092ff1e13da85168a87b57934b7c45c
Zackary Lowery <zlowery@xcjs.com>
2016-03-19 02:16:08 -0400

    Moved all statements in function.js to inside the closure.

--- www/src/js/function.js
+++ www/src/js/function.js
@@ -35,12 +35,12 @@
         	});
         });
     }
+
+    $('.bg-1,.bg-3').parallax({
+    	speed :	0.15
+    });
+
+    $('.bg-2').parallax({
+    	speed :	0.25
+    });
 }(jQuery));
-
-$('.bg-1,.bg-3').parallax({
-	speed :	0.15
-});
-
-$('.bg-2').parallax({
-	speed :	0.25
-});
